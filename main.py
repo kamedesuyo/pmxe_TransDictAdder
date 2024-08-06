8929d4a3bbf29304f1d180b60cb436bdd85c168e
Nagata Toshiya <gp23a103@oecu.jp>
2024-08-06 13:06:30 +0900

    using pymeshio

--- main.py
+++ main.py
@@ -2,26 +2,47 @@ import pymeshio.pmx.reader as pmxreader
 import glob
 import os
 
-# 拡張子の設定
 file_extension = '*.pmx'
-
-# 対象ファイルの取得
 directory_path = "pmx"
-
-# 拡張子のファイルを取得
 file_list = glob.glob(os.path.join(directory_path, file_extension))
-
 error_list = []
-material_list = []
-# ファイルの読み込み
-for file in file_list:
-    file.replace('\\', '/')
-    try:
-        pmx = pmxreader.read_from_file(file)
-        for material in pmx.materials:
-            material_list.append(material.name)
-    except Exception as e:
-        print(e)
-        error_list.append(file)
-print(set(material_list))
-print(str(error_list)+"でエラーが発生しました。")
+
+def get_material_list(material_list:list) -> list:
+    for file in file_list:
+        file.replace('\\', '/')
+        try:
+            pmx = pmxreader.read_from_file(file)
+            for material in pmx.materials:
+                material_list.append(material.name)
+        except Exception as e:
+            error_list.append(f"{file}でエラーが発生しました")
+    return material_list
+
+
+def write_material_names(material_list: list) -> None:
+    with open('material_list.txt', 'w',encoding="utf-8") as f:    
+        for material in material_list:
+            try:
+                f.write(f"{material}\n")
+            except Exception as e:
+                error_list.append(f"ファイル書き込み中にエラーが発生しました:{e}")
+
+def show_error():
+    for error in error_list:
+        print(error)
+
+# メイン処理
+def main():
+    # マテリアル名取得処理
+    material_list = []
+    get_material_list(material_list)
+    material_list = list(set(material_list))
+    material_list = sorted(material_list)
+    write_material_names(material_list)
+    show_error()
+
+    # 辞書作成処理
+    
+
+if __name__ == '__main__':
+    main()
