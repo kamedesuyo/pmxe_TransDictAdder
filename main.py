b42248e933a79cdd3083948246a7cb3ca89b8bab
Nagata Toshiya <gp23a103@oecu.jp>
2024-08-10 11:59:44 +0900

    [clean]pathのチェックをきれいにしました。

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import pymeshio.pmx.reader as pmxreader
 import glob
 import os
+import sys
 
 # 各種パス
 material_list_path = "material_list.txt"
 material_trans_list_path = "material_trans_list.txt"
 dictionary_path = "dictionary.txt"
 dict_path = "dict_path.txt"
-check_list_path = [material_list_path, material_trans_list_path,dict_path]
+check_list_path = [material_list_path, material_trans_list_path,dictionary_path]
 
 dictionary_data = {}     # 辞書データ
 error_list = []          # pmxロード時エラーリスト
 material_list = []       # マテリアル名リスト
 material_trans_list = [] # 翻訳結果リスト
 
+
+# dict_pathのチェック
+# try get dict path -> 存在しない or 空の場合、dictionary.txtを作成
+if not os.path.exists(dict_path):
+    open(dict_path, 'w', encoding="utf-8").close()
+elif os.path.getsize(dict_path) != 0:
+    with open("dict_path.txt", 'r', encoding="utf-8") as f:
+        path = f.read().replace(" ", "").replace("\"","")
+        if os.path.exists(path):
+            dictionary_path = path
+            
 # 各種list_pathのチェック
 # list,trans_listファイルが存在しない場合、空ファイルを作成
 for path in check_list_path:
     if not os.path.exists(path):
+        print(f"{path}が見つかりませんでした。同階層に生成します。")
         open(path, 'w', encoding="utf-8").close()
-
-# dict_pathのチェック
-# try get dict path -> 存在しない or 空の場合、dictionary.txtを作成
-with open("dict_path.txt", 'r', encoding="utf-8") as f:
-    path = f.read().replace(" ", "").replace("\"","")
-    # パス設定が存在し、ファイルが存在する場合、dictionary_pathを更新
-    if 0 < len(path) and os.path.exists(path):
-        dictionary_path = path
-    # パス設定が存在するが、ファイルが存在しない場合、dictionary.txtを作成
-    elif 0 < len(path) and not os.path.exists(path):
-        print(f"dictionary.txtが見つかりませんでした。")
-        print(f"{dictionary_path}を使用します。")
-        
 
 # 終了処理
 def exit_process():
     input("\nenterキーを押して終了...")
-    os.remove(material_list_path)
-    os.remove(material_trans_list_path)
-    os._exit(-1)
+    sys.exit(0)
 
 # pmxファイルロード時のエラーメッセージ表示
 def show_load_error():
@@ -48,19 +46,14 @@ def show_load_error():
 # dictionary.txtのデータ取得
 def get_dictionary_data():
     global dictionary_data
-    try:
-        with open(dictionary_path, "r+", encoding="utf-8") as f:
-            dictionary_list = f.read().replace(" ", "").split("\n")
-        # dictionary.txtのデータを辞書型に変換
-        for i in range(len(dictionary_list)-1):
-            k = dictionary_list[i].split(",")[0]
-            v = dictionary_list[i].split(",")[1]
-            dictionary_data[k] = v
+    with open(dictionary_path, "r+", encoding="utf-8") as f:
+        dictionary_list = f.read().replace(" ", "").split("\n")
+    # dictionary.txtのデータを辞書型に変換
+    for i in range(len(dictionary_list)-1):
+        k = dictionary_list[i].split(",")[0]
+        v = dictionary_list[i].split(",")[1]
+        dictionary_data[k] = v
 
-    except FileNotFoundError:
-        print("dictionary.txtが見つかりませんでした。")
-        print("同階層にdictionary.txtを生成して続行します。")
-        open(dictionary_path, "w", encoding="utf-8").close()
 #pmxファイルからマテリアル名取得
 def get_material_list():
     global material_list
@@ -78,12 +71,12 @@ def get_material_list():
             for material in pmx.materials:
                 if material.name not in dictionary_data.keys():
                     material_list.append(material.name)
-            if len(material_list) == 0:
-                print("追加するマテリアル名が見つかりませんでした。\npmxフォルダ内にpmmが含まれているか確認してください。")
-                exit_process()
             material_list = sorted(list(set(material_list)))
         except Exception as e:
             error_list.append(f"{file}の読み込みに失敗しました。")
+    if len(material_list) == 0:
+        print("追加するマテリアル名が見つかりませんでした。\npmxフォルダ内にpmmが含まれているか確認してください。")
+        exit_process()
         
 # 取得したmaterial_listをmaterial_list.txtに書き出す
 def write_material_names() -> None:
@@ -128,7 +121,7 @@ def main():
     get_material_list()
     material_list = sorted(list(set(material_list)))
     write_material_names()
-    print(f"マテリアル名取得処理が完了しました。追加予定:\n{material_list}\n")
+    print(f"追加予定:\n{material_list}\n")
     show_load_error()
 
     # material_trans_list.txt取得、辞書作成
