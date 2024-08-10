0347b2a085f0dcce74ef983c94594b29a03c24a9
Nagata Toshiya <gp23a103@oecu.jp>
2024-08-10 19:11:45 +0900

    [init]ファイル関係を整理

--- TransDictAdder/validation/material_valid.py
+++ TransDictAdder/validation/material_valid.py
@@ -3,8 +3,3 @@ def verify_materials(materials,trans_materials)->bool:
         print("材質名の数が一致しません。")
         return False
     return True
-
-def verify_load_materials(materials:dict):
-    if not materials or len(materials) == 0:
-        print("追加する材質名が見つかりませんでした。")
-        return
